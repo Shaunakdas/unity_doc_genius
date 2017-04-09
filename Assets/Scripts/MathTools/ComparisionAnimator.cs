9035acf0258d97df8a9544166d2ac2365f468556
Shaunakdas <shaunakdas2020@gmail.com>
2017-04-10 00:31:23 +0530

    Comparision E2E finished

---
+++ Assets/Scripts/MathTools/ComparisionAnimator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ComparisionAnimator : MonoBehaviour {
+	public GameObject NumberTableGO;
+	public GameObject TextCellPrefab;
+	public List<int> InputList;
+	public List<List<GameObject>> InputGOList;
+	// Use this for initialization
+	void Start () {
+		if (InputList.Count > 0) {
+			//for Debugging purpose
+			initiateInputList(InputList);
+		}
+	}
+	public void initiateInputList(List<int> currInputList){
+		ComparisionController comparisionCtrl = new ComparisionController(currInputList);
+		//Updating NumberLocationList with new inputList
+		//Filling TableGO with Number at their location wrt straight Number Location List
+		InputGOList = new List<List<GameObject>>();
+		updateTableGO(comparisionCtrl.numberLocationList);
+		NumberTableGO.GetComponent<UITable> ().columns = comparisionCtrl.tableColumnCount;
+		animationStepList (comparisionCtrl);
+	}
+	public void updateTableGO(List<List<int?>> numberLocationList){
+		NumberTableGO.transform.DestroyChildren ();
+		for (int i = 0; i < numberLocationList.Count; i++) {
+			List<GameObject> numberGORow = new List<GameObject> ();
+			for (int j = 0; j < numberLocationList [i].Count; j++) {
+				GameObject textCell = NGUITools.AddChild (NumberTableGO, TextCellPrefab);
+				if (numberLocationList [i] [j].HasValue) {
+					textCell.GetComponent<UILabel> ().text = numberLocationList [i] [j].ToString ();
+					Debug.Log (i + " " + j+"  "+numberLocationList [i] [j].ToString());
+				}
+				numberGORow.Add (textCell);
+			}
+			InputGOList.Add (numberGORow);
+		}
+
+	}
+	public void animationStepList(ComparisionController comparisionCtrl){
+		Debug.Log ("AnimationStepList");
+		int column = comparisionCtrl.getTableColumnCount ();
+		for(int i = 1;i<column;i++){
+			animationStep (comparisionCtrl,i);
+		}
+	}
+	public void animationStep (ComparisionController comparisionCtrl, int stepIndex){
+		Debug.Log ("animationStep at column is "+stepIndex);
+		//highlight existing numbers at column index 
+		foreach (List<int?> numberRow in comparisionCtrl.numberLocationList){
+			if (numberRow [stepIndex].HasValue) {
+			}
+		}
+		//Display sum and carry by updating TableGO
+		updateTableGO(comparisionCtrl.compareNumberLocationList(stepIndex));
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MathTools/ComparisionController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class ComparisionController {
+	//25+3=24: 25-Augend, 3-Addend, 75-Sum
+	//For holding input integer list for addition
+	public List<int> inputList{ get; set; }
+	public int rowCount{ get; set;}
+	//For holding integer location during addition
+	public List<List<int?>> numberLocationList{ get; set; }
+
+	//For Table Column count
+	public int tableColumnCount{get; set;}
+	public ComparisionController(List<int> currInputList){
+		inputList = currInputList;
+		tableColumnCount = getTableColumnCount ();
+		numberLocationList = new List<List<int?>> ();
+		setNumberLocationList ();
+		rowCount = inputList.Count + 1;
+	}
+	public int getTableColumnCount(){
+		//Get Column Count for UITable 
+		//Adding One column to carry after largest column
+		int columnCount = inputList.Max().ToString().Count();
+		return columnCount+1;
+	}
+
+	public List<List<int?>> setNumberLocationList(){
+		//Adding 2 to inputList.Count to handle carry row and sum row
+		//Removing last row (sum row)from calculations
+		for(int rowCount=0; rowCount< (inputList.Count); rowCount++){
+			List<int?> numberLocationRow = new List<int?> ();
+			numberLocationRow.Add(inputList[rowCount]);
+			for (int columnCount = 1; columnCount < tableColumnCount; columnCount++) {
+				int inputLength = inputList [rowCount].ToString ().Length;
+				int startIndex = tableColumnCount - inputLength;
+				if (columnCount >= startIndex) {
+					numberLocationRow.Add (int.Parse (inputList [rowCount].ToString () [columnCount - startIndex].ToString ()));
+				} else {
+					numberLocationRow.Add(null);
+				}
+
+			}
+			numberLocationList.Add (numberLocationRow);
+		}
+		return numberLocationList;
+	}
+
+	public List<List<int?>> compareNumberLocationList(int operationColumn){
+		int div = (int)Mathf.Pow( 10,(tableColumnCount - operationColumn - 1));
+		if (div == 1)
+			div = 0;
+		Debug.Log ("Div id " + div.ToString());
+//		numberLocationList.OrderBy (x => x [0] % div);
+		numberLocationList.Sort((x,y)=>digitComparer((int)x[0],div).CompareTo(digitComparer((int)y[0],div)));
+		return numberLocationList;
+	}
+	public int digitComparer(int integer, int div){
+		return integer - (integer % div);
+	}
+}
