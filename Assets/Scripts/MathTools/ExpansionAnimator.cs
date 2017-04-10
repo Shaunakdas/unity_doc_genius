5eaefceb5887320a94f36ecf5691951b26cfa44e
Shaunakdas <shaunakdas2020@gmail.com>
2017-04-10 16:25:12 +0530

    Expansion e2E finished

---
+++ Assets/Scripts/MathTools/ExpansionAnimator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ExpansionAnimator : MonoBehaviour {
+	public GameObject NumberTableGO;
+	public GameObject TextCellPrefab;
+	public GameObject AnswerGO;
+	public int InputNumber;
+	public List<int?> singleNumberLocationList{ get; set; }
+	// Use this for initialization
+	void Start () {
+			//for Debugging purpose
+			initiateInputList(InputNumber);
+	}
+	public void initiateInputList(int currInputNumber){
+		ExpansionController expansionCtrl = new ExpansionController(currInputNumber);
+		//Updating NumberLocationList with new inputList
+		//Filling TableGO with Number at their location wrt straight Number Location List
+		singleNumberLocationList = expansionCtrl.singleNumberLocationList;
+		updateTableGO(singleNumberLocationList);
+		NumberTableGO.GetComponent<UIGrid> ().maxPerLine = expansionCtrl.tableColumnCount;
+		animationStepList (expansionCtrl);
+	}
+	public void updateTableGO(List<int?> singleNumberLocationList){
+		NumberTableGO.transform.DestroyChildren ();
+		GameObject inputNumberCell = NGUITools.AddChild (NumberTableGO, TextCellPrefab);
+		inputNumberCell.GetComponent<UILabel> ().text = InputNumber.ToString();
+		GameObject equalSignCell = NGUITools.AddChild (NumberTableGO, TextCellPrefab);
+		equalSignCell.GetComponent<UILabel> ().text = "=";
+
+		//Check if any int? has value, based on it, fill the cell
+		foreach (int? number in singleNumberLocationList) {
+			GameObject textCell = NGUITools.AddChild (NumberTableGO, TextCellPrefab);
+			if (number.HasValue) {
+				Debug.Log ("Number is" + number);
+				textCell.GetComponent<UILabel> ().text = number.ToString ();
+			} else {
+				textCell.GetComponent<UILabel> ().text = ",";
+			}
+		}
+
+	}
+	public void animationStepList(ExpansionController expansionCtrl){
+		Debug.Log ("AnimationStepList"+expansionCtrl.getCommaCount());
+		int column = Mathf.Min(expansionCtrl.comparisionTermIndexList.Count,expansionCtrl.getCommaCount());
+		for(int i = 0;i<column;i++){
+			commaAnimationStep (expansionCtrl,i);
+			updateAnswerGO ();
+		}
+		AnswerGO.GetComponent<UILabel> ().text = HumanFriendlyInteger.NumberToWords (InputNumber);
+	}
+	public void commaAnimationStep (ExpansionController expansionCtrl, int stepIndex){
+		Debug.Log ("animationStep at column is "+stepIndex);
+		//highlight existing numbers at column index 
+		GameObject targetGO = NumberTableGO.transform.GetChild (0).gameObject;
+		int comparisionTermIndex = expansionCtrl.comparisionTermIndexList [stepIndex];
+		//Display sum and carry by updating TableGO
+		singleNumberLocationList = expansionCtrl.updateNumberLocationList(stepIndex);
+		updateTableGO(singleNumberLocationList);
+	}
+	public void updateAnswerGO (){
+		string number = "";
+		List<int?> fullTextLocationList = new List<int?> (singleNumberLocationList);
+		fullTextLocationList.RemoveAt (0);
+		singleNumberLocationList.ForEach (item => number += toText(item));
+		AnswerGO.GetComponent<UILabel> ().text = HumanFriendlyInteger.NumberToWords (int.Parse (number));
+	}
+	public string toText(int? item){
+		if (item.HasValue)
+			return item.ToString ();
+		else
+			return "";
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MathTools/ExpansionController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class ExpansionController {
+	//25+3=24: 25-Augend, 3-Addend, 75-Sum
+	//For holding input integer list for addition
+	public int inputNumber{ get; set; }
+	//For holding integer location during addition
+	public List<int?> singleNumberLocationList{ get; set; }
+	public List<string> comparisionTermList = new List<string>(new string[]{"Thousand","Lakhs","Crores"});
+	public List<int> comparisionTermIndexList = new List<int>(new int[]{3,2,2});
+	//For Table Column count
+	public int tableColumnCount{get; set;}
+	public ExpansionController(int currInputNumber){
+		inputNumber = currInputNumber;
+		tableColumnCount = getTableColumnCount ();
+		singleNumberLocationList = new List<int?> ();
+		setNumberLocationList ();
+	}
+	public int getTableColumnCount(){
+		//Get Column Count for UITable 
+		int columnCount = 2*inputNumber.ToString().Count();
+		return columnCount;
+	}
+	public int getCommaCount(){
+		if ((inputNumber / ((int)Mathf.Pow (10, 7))) > 0)
+			return 3;
+		else if ((inputNumber / ((int)Mathf.Pow (10, 5))) > 0)
+			return 2;
+		else if ((inputNumber / ((int)Mathf.Pow (10, 3))) > 0)
+			return 1;
+		else
+			return 0;
+	}
+	public List<int?> setNumberLocationList(int currInputNumber,int currTableColumnCount){
+		//Get numberLocationList
+		inputNumber = currInputNumber;
+		return setNumberLocationList ();
+	}
+	public List<int?> setNumberLocationList(){
+		singleNumberLocationList.Add (inputNumber);
+		return singleNumberLocationList;
+	}
+	public List<int?> updateNumberLocationList(int comparisionTermIndex){
+		//OperationColumn is from right hand side
+
+		string comparisionTerm = comparisionTermList [comparisionTermIndex];
+		int comparisionTermLoc = comparisionTermIndexList [comparisionTermIndex];
+		int targetNumber = (int)singleNumberLocationList [0];
+		int targetLength = targetNumber.ToString ().Count();
+		singleNumberLocationList.RemoveAt(0);
+		Debug.Log ( targetLength - comparisionTermLoc);
+		Debug.Log (targetNumber.ToString ().Substring (0, targetLength - comparisionTermLoc));
+		singleNumberLocationList.Insert (0, int.Parse(targetNumber.ToString ().Substring (0,targetLength - comparisionTermLoc)));
+		singleNumberLocationList.Insert (1, null);
+		singleNumberLocationList.Insert (2, int.Parse(targetNumber.ToString ().Substring (targetLength - comparisionTermLoc,comparisionTermLoc)));
+		return singleNumberLocationList;
+
+	}
+}
